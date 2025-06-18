7e60a011abb6f53e15b5c16f3c05178464c79a8b
Diego <diegosantos4957@gmail.com>
2025-06-17 22:21:24 -0300

    feat: remove console

--- src/shared/prisma/prisma.connect.ts
+++ src/shared/prisma/prisma.connect.ts
@@ -5,6 +5,5 @@ import { PrismaClient } from '../../../prisma/generated/client/postgres';
 export class PrismaConnect extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
-    console.log('Postgres connected');
   }
 }
