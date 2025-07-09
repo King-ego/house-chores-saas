aa688f153ea833d5e33b9f5820b32bf132978934
Diego <diegosantos4957@gmail.com>
2025-07-09 11:36:26 -0300

    feat: create router get by user id

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,14 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { CreateUsersService } from './services/create.users.service';
+import { User } from '../../../prisma/generated/client/postgres';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly createUsersService: CreateUsersService) {}
+
   @Get()
   findAll(): string {
     return 'This action returns all users12';
   }
 
   @Get(':id')
-  findOne(): string {
-    return 'This action returns a user';
+  public async findOne(): Promise<User> {
+    const user = await this.createUsersService.execute('1');
+    return user;
   }
 }
