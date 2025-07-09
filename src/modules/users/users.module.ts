05c91a32fe53e25f057c4c07aa6a6577adbcbec9
Diego <diegosantos4957@gmail.com>
2025-07-09 11:36:44 -0300

    feat: update module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
+import { UsersServicesModule } from './users.services.module';
 
 @Module({
   controllers: [UsersController],
+  providers: [],
+  imports: [UsersServicesModule],
 })
 export class UsersModule {}
