4980a800c59dbb816765a31fe5f2709a5f76d9b4
Diego <diegosantos4957@gmail.com>
2025-07-09 11:37:36 -0300

    feat: create service for get user by id

---
+++ src/modules/users/services/create.users.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { UsersRepository } from '../repositories/users.repository';
+import { User } from '../../../../prisma/generated/client/postgres';
+
+@Injectable()
+export class CreateUsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  public async execute(id: string): Promise<User> {
+    return this.usersRepository.findByFilter(id);
+  }
+}
