719044c01f5571aecb25e055d87a156b7ffecbe5
Diego <diegosantos4957@gmail.com>
2025-07-11 06:23:46 -0300

    feat: insert module property in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './shared/prisma/prisma.module';
 import { UsersModule } from './modules/users/users.module';
+import { PropertiesModule } from './modules/properties/properties.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, PropertiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
