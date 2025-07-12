eb0abfc0552a2436bb1e93eef81410091c1550cb
Diego <diegosantos4957@gmail.com>
2025-07-12 20:57:49 -0300

    feat: repository by property

---
+++ src/modules/properties/repositories/property.repositories.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PostgresClient, PrismaOrm } from '../../../shared/prisma/prisma.orm';
+import PropertyContractor from '../contractors/property.contractor';
+import CreatePropertyInput from '../contractors/inputs/create.property.input';
+
+@Injectable()
+export class PropertyRepositories implements PropertyContractor {
+  private readonly postgresOrm: PostgresClient;
+
+  constructor() {
+    this.postgresOrm = new PrismaOrm().prismaPostgres();
+  }
+
+  public async create_property(property: CreatePropertyInput): Promise<void> {
+    await this.postgresOrm.property.create({
+      data: property,
+    });
+  }
+}
