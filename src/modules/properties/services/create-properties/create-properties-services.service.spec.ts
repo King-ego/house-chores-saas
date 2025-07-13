26a285347f9f016407b1210a4d0f9decde80d4ee
Diego <diegosantos4957@gmail.com>
2025-07-12 21:56:33 -0300

    feat: create service add new users

---
+++ src/modules/properties/services/create-properties/create-properties-services.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CreatePropertiesServicesService } from './create-properties-services.service';
+
+describe('CreatePropertiesServicesService', () => {
+  let service: CreatePropertiesServicesService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [CreatePropertiesServicesService],
+    }).compile();
+
+    service = module.get<CreatePropertiesServicesService>(CreatePropertiesServicesService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
---
+++ src/modules/properties/services/create-properties/create-properties-services.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PropertyRepositories } from '../../repositories/property.repositories';
+
+interface CreatePropertyRequest {
+  name: string;
+  description?: string;
+  created_by: string;
+}
+
+@Injectable()
+export class CreatePropertiesServicesService {
+  constructor(
+    private readonly propertiesRepository: PropertyRepositories
+  ) {
+  }
+
+  public async execute(property: CreatePropertyRequest): Promise<void> {
+    await this.propertiesRepository.create_property(property);
+  }
+}
