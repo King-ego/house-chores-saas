1cd3fa5250dbddfbab84ed2fd7b7ffc3f9678553
Diego <diegosantos4957@gmail.com>
2025-07-13 18:00:57 -0300

    feat: update module user

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersRepositoryModule } from './repositories/users.repository.module';
-import { ListUsersByIdService } from './services/list.users.by.id.service';
+import { ListUsersByIdService } from './services/list-users-by-id/list-users-by-id.service';
+import { CreateUserService } from './services/create-user/create-user.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [ListUsersByIdService],
+  providers: [ListUsersByIdService, CreateUserService],
   exports: [ListUsersByIdService],
   imports: [UsersRepositoryModule],
 })
