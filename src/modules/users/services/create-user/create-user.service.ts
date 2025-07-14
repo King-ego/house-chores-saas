c58d19b5599ef36ea6d622beaebb7c0d79d1e793
Diego <diegosantos4957@gmail.com>
2025-07-14 18:37:29 -0300

    feat: service create user

--- src/modules/users/services/create-user/create-user.service.ts
+++ src/modules/users/services/create-user/create-user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from '../../repositories/users.repository';
+
+interface CreateUserRequest {
+  name: string;
+  email: string;
+}
 
 @Injectable()
-export class CreateUserService {}
+export class CreateUserService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  public async execute(user: CreateUserRequest): Promise<void> {
+    await this.usersRepository.createUser(user);
+  }
+}
