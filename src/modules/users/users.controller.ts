70c4298cd6887d6fd6cd15a06c19975a73537b66
Diego <diegosantos4957@gmail.com>
2025-07-14 18:33:07 -0300

    feat: create route create user

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ListUsersByIdService } from './services/list-users-by-id/list-users-by-id.service';
 import { User } from '../../../prisma/generated/client/postgres';
+import { CreateUserService } from './services/create-user/create-user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly createUsersService: ListUsersByIdService) {}
+  constructor(
+    private readonly createUsersService: CreateUserService,
+    private readonly listUsersService: ListUsersByIdService,
+  ) {}
 
   @Get()
   findAll(): string {
@@ -13,7 +18,18 @@ export class UsersController {
 
   @Get(':id')
   public async findOne(): Promise<User> {
-    const user = await this.createUsersService.execute('1');
+    const user = await this.listUsersService.execute('1');
     return user;
+  }
+
+  @Post('/create')
+  public async createUser(@Body() createUser: CreateUserDto) {
+    const user = {
+      name: createUser.name,
+      email: createUser.email,
+    };
+
+    await this.createUsersService.execute(user);
+    return { message: 'This action creates a user' };
   }
 }
