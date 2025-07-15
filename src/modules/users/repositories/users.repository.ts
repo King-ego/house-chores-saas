5ced0544d93c995b63f3f29266be54198d7900fa
Diego <diegosantos4957@gmail.com>
2025-07-15 20:21:53 -0300

    feat: method create user in user repository

--- src/modules/users/repositories/users.repository.ts
+++ src/modules/users/repositories/users.repository.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaOrm, PostgresClient } from '../../../shared/prisma/prisma.orm';
 import { User } from '../../../../prisma/generated/client/postgres';
+import { UserContractor } from '../contractors/user.contractor';
+import { CreateUserInput } from '../contractors/inputs/create-user-input';
 
 @Injectable()
-export class UsersRepository {
+export class UsersRepository implements UserContractor {
   private readonly postgresOrm: PostgresClient;
 
   constructor() {
@@ -16,5 +18,11 @@ export class UsersRepository {
       FROM users
       WHERE users.id = ${id};
     `;
+  }
+
+  public async createUser(user: CreateUserInput): Promise<void> {
+    await this.postgresOrm.user.create({
+      data: user,
+    });
   }
 }
