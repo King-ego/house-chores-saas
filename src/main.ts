55fdd6e93914b686fac263d5a9c8333bdc0c576b
Diego <diegosantos4957@gmail.com>
2025-07-17 19:08:43 -0300

    feat: error git file migration errors

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './shared/errors/allExceptionsFilter';
+import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
+  console.log({ port: process.env.PORT });
   await app.listen(process.env.PORT ?? 8000);
 }
 
