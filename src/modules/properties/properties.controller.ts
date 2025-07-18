a669b2f5ba443228c8c95d8e96c4c7fa6a778ace
Diego <diegosantos4957@gmail.com>
2025-07-18 17:36:15 -0300

    feat: route create property

--- src/modules/properties/properties.controller.ts
+++ src/modules/properties/properties.controller.ts
@@ -1,10 +1,23 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreatePropertyDto } from './dto/createPropertyDto';
+import { CreatePropertiesServicesService } from './services/create-properties/create-properties-services.service';
 
 @Controller('properties')
 export class PropertiesController {
+  constructor(
+    private readonly createPropertiesServicesService: CreatePropertiesServicesService,
+  ) {}
+
   @Post('')
-  createProperty() {
-    // Logic to create a property
+  public async createProperty(@Body() createPropertyDto: CreatePropertyDto) {
+    const property = {
+      name: createPropertyDto.name,
+      description: createPropertyDto.description,
+      created_by: createPropertyDto.created_by,
+    };
+
+    await this.createPropertiesServicesService.execute(property);
+
     return { message: 'Property created successfully' };
   }
 }
