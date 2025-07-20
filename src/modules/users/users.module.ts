5b6d795d938245056731cfdbd057d95000b19e59
Diego <diegosantos4957@gmail.com>
2025-07-19 21:04:17 -0300

    feat: list service in module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
+import { UsersController } from './controllers/users/users.controller';
 import { UsersRepositoryModule } from './repositories/users.repository.module';
 import { ListUsersByIdService } from './services/list-users-by-id/list-users-by-id.service';
 import { CreateUserService } from './services/create-user/create-user.service';
+import { InviteUserService } from './services/invite-user/invite-user.service';
+import { InviteUsersController } from './controllers/invite-users/invite-users.controller';
 
 @Module({
-  controllers: [UsersController],
-  providers: [ListUsersByIdService, CreateUserService],
-  exports: [ListUsersByIdService],
+  controllers: [UsersController, InviteUsersController],
+  providers: [ListUsersByIdService, CreateUserService, InviteUserService],
   imports: [UsersRepositoryModule],
 })
 export class UsersModule {}
