d9223786d97e81265992de7300feec60781b0cd1
Diego <diegosantos4957@gmail.com>
2025-07-20 21:13:17 -0300

    refactor: remove console

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
-  console.log({ port: process.env.PORT });
   await app.listen(process.env.PORT ?? 8000);
 }
 
