baa523a67cada7ff5e0527d81443c680cbfcf839
Diego <diegosantos4957@gmail.com>
2025-07-23 19:16:54 -0300

    feat: create route get notifications

--- src/modules/notifications/controllers/notifications/notifications.controller.ts
+++ src/modules/notifications/controllers/notifications/notifications.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('notifications')
-export class NotificationsController {}
+export class NotificationsController {
+  @Get('/:userId')
+  public notifications(@Param('userId') userId: string) {
+    return {
+      message: 'Notifications endpoint',
+      userId,
+    };
+  }
+}
