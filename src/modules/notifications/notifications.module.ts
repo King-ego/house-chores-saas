36465eccbec6394e16aba688b633af6f2a6399d6
Diego <diegosantos4957@gmail.com>
2025-07-23 19:30:57 -0300

    feat: change module notifications

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './controllers/notifications/notifications.controller';
 import { ListNotificationsIdService } from './services/list-notifications-id/list-notifications-id.service';
+import { NotificationsRepositoryModule } from './repositories/notifications.repository.module';
 
 @Module({
   controllers: [NotificationsController],
-  providers: [ListNotificationsIdService]
+  providers: [ListNotificationsIdService],
+  imports: [NotificationsRepositoryModule],
 })
 export class NotificationsModule {}
