733869f5c312e03eb24b634125111bdfa4516009
Diego <diegosantos4957@gmail.com>
2025-07-24 18:01:22 -0300

    feat: import notifications repository module

--- src/modules/notifications/repositories/notifications.repository.module.ts
+++ src/modules/notifications/repositories/notifications.repository.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { NotificationsService } from './notifications/notifications.service';
 
-@Module({})
+@Module({
+  providers: [NotificationsService],
+  exports: [NotificationsService],
+})
 export class NotificationsRepositoryModule {}
