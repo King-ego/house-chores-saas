65884ff53819805a9abee7efe2115d7d43e74fab
Diego <diegosantos4957@gmail.com>
2025-07-24 18:00:59 -0300

    feat: create notifications repository

--- src/modules/notifications/repositories/notifications/notifications.service.ts
+++ src/modules/notifications/repositories/notifications/notifications.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Notification } from '../../../../../prisma/generated/client/postgres';
+
+import {
+  PostgresClient,
+  PrismaOrm,
+} from '../../../../shared/prisma/prisma.orm';
 
 @Injectable()
-export class NotificationsService {}
+export class NotificationsService {
+  private readonly postgresOrm: PostgresClient;
+
+  constructor() {
+    this.postgresOrm = new PrismaOrm().prismaPostgres();
+  }
+
+  public async listNotificationsByUserId(
+    userId: string,
+  ): Promise<Notification[]> {
+    return this.postgresOrm.notification.findMany({
+      where: { user_id: userId },
+    });
+  }
+}
