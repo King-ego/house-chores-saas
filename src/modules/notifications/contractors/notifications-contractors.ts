c28ec9284355f82fcbc92886243289ff30ab97cf
Diego <diegosantos4957@gmail.com>
2025-07-25 18:37:05 -0300

    feat: new method create in contractor notification

--- src/modules/notifications/contractors/notifications-contractors.ts
+++ src/modules/notifications/contractors/notifications-contractors.ts
@@ -1,5 +1,7 @@
 import { Notification } from '../../../../prisma/generated/client/postgres';
+import { CreateNotificationInput } from './inputs/create.notification.input';
 
 export interface NotificationsContractors {
   listNotificationsByUserId(userId: string): Promise<Notification[]>;
+  createNotification(_: CreateNotificationInput): Promise<void>;
 }
