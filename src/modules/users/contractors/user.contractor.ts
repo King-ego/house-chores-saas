8cdc8911b3f2b814a9fae048a60003402f5a1e91
Diego <diegosantos4957@gmail.com>
2025-07-26 12:46:07 -0300

    feat: add return user in contractor user repository

--- src/modules/users/contractors/user.contractor.ts
+++ src/modules/users/contractors/user.contractor.ts
@@ -3,5 +3,5 @@ import { CreateUserInput } from './inputs/create-user-input';
 
 export class UserContractor {
   findByFilter: (userId: string) => Promise<User>;
-  createUser: (userData: CreateUserInput) => Promise<void>;
+  createUser: (userData: CreateUserInput) => Promise<User>;
 }
