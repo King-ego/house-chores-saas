b01151a57a1f96143ae79af9c11682554b55ac10
Diego <diegosantos4957@gmail.com>
2025-07-26 17:19:49 -0300

    feat: return property in property repository

--- src/modules/properties/repositories/properties/property.repositories.ts
+++ src/modules/properties/repositories/properties/property.repositories.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { PostgresClient, PrismaOrm } from '../../../../shared/prisma/prisma.orm';
+import {
+  PostgresClient,
+  PrismaOrm,
+} from '../../../../shared/prisma/prisma.orm';
 import PropertyContractor from '../../contractors/property.contractor';
 import CreatePropertyInput from '../../contractors/inputs/create.property.input';
+import { Property } from '../../../../../prisma/generated/client/postgres';
 
 @Injectable()
 export class PropertyRepositories implements PropertyContractor {
@@ -11,8 +15,10 @@ export class PropertyRepositories implements PropertyContractor {
     this.postgresOrm = new PrismaOrm().prismaPostgres();
   }
 
-  public async create_property(property: CreatePropertyInput): Promise<void> {
-    await this.postgresOrm.property.create({
+  public async create_property(
+    property: CreatePropertyInput,
+  ): Promise<Property> {
+    return this.postgresOrm.property.create({
       data: {
         ...property,
         users: {
