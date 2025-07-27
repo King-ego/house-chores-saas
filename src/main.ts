5ad78fa314846eb5d1cb9ad654ff3e5f5ab34ee5
Diego <diegosantos4957@gmail.com>
2025-07-27 19:52:48 -0300

    feat: verify port in env

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
+  console.log(process.env.PORT);
   await app.listen(process.env.PORT ?? 8000);
 }
 
