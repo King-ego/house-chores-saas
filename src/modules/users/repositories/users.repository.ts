8a4b48850d350d6d3ef2659b6cd57fa92d8ef613
Diego <diegosantos4957@gmail.com>
2025-07-28 18:02:09 -0300

    feat: params update find filter repository

--- src/modules/users/repositories/users.repository.ts
+++ src/modules/users/repositories/users.repository.ts
@@ -3,6 +3,7 @@ import { PrismaOrm, PostgresClient } from '../../../shared/prisma/prisma.orm';
 import { User } from '../../../../prisma/generated/client/postgres';
 import { UserContractor } from '../contractors/user.contractor';
 import { CreateUserInput } from '../contractors/inputs/create-user-input';
+import { FindByFilterUserInput } from '../contractors/inputs/find-by-filter-user-input';
 
 @Injectable()
 export class UsersRepository implements UserContractor {
@@ -12,11 +13,14 @@ export class UsersRepository implements UserContractor {
     this.postgresOrm = new PrismaOrm().prismaPostgres();
   }
 
-  public async findByFilter(id: string): Promise<User> {
+  public async findByFilter(data: FindByFilterUserInput): Promise<User> {
+    const { id, email } = data;
     return this.postgresOrm.$queryRaw`
       SELECT *
       FROM users
-      WHERE users.id = ${id};
+      WHERE 
+        users.id = ${id}
+        OR users.email = ${email};
     `;
   }
 
