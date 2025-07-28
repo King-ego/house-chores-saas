6c800c08adf80c7d515fa100b71a89bf065fc05b
Diego <diegosantos4957@gmail.com>
2025-07-28 18:01:49 -0300

    feat: params update

--- src/modules/users/contractors/user.contractor.ts
+++ src/modules/users/contractors/user.contractor.ts
@@ -1,7 +1,8 @@
 import { User } from '../../../../prisma/generated/client/postgres';
 import { CreateUserInput } from './inputs/create-user-input';
+import { FindByFilterUserInput } from './inputs/find-by-filter-user-input';
 
 export class UserContractor {
-  findByFilter: (userId: string) => Promise<User>;
+  findByFilter: (_: FindByFilterUserInput) => Promise<User>;
   createUser: (userData: CreateUserInput) => Promise<User>;
 }
