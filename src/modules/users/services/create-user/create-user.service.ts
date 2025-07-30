40b528e6e1b4cfb0d4b28130702cd0785385df70
Diego <diegosantos4957@gmail.com>
2025-07-29 22:03:05 -0300

    feat: add password in service create user

--- src/modules/users/services/create-user/create-user.service.ts
+++ src/modules/users/services/create-user/create-user.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from '../../repositories/users.repository';
 import { User } from '../../../../../prisma/generated/client/postgres';
+import { PasswordService } from '../password/password.service';
 
 interface CreateUserRequest {
   name: string;
   email: string;
+  password: string;
 }
 
 @Injectable()
 export class CreateUserService {
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly passwordService: PasswordService,
+  ) {}
 
   public async execute(user: CreateUserRequest): Promise<User> {
+    user.password = await this.passwordService.hashPassword(user.password);
     return this.usersRepository.createUser(user);
   }
 }
