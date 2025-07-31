37f38bf2b1377b3becb26d574de7738fad2982af
Diego <diegosantos4957@gmail.com>
2025-07-30 21:26:24 -0300

    feat: add list property by user id in property module

--- src/modules/properties/properties.module.ts
+++ src/modules/properties/properties.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PropertiesController } from './controllers/properties/properties.controller';
 import { CreatePropertiesService } from './services/create-properties/create-properties.service';
 import { PropertiesRepositoryModule } from './repositories/properties.repository.module';
+import { ListPropertyByUserIdService } from './services/list-property-by-user-id/list-property-by-user-id.service';
 
 @Module({
   controllers: [PropertiesController],
-  providers: [CreatePropertiesService],
+  providers: [CreatePropertiesService, ListPropertyByUserIdService],
   imports: [PropertiesRepositoryModule],
 })
 export class PropertiesModule {}
