9e80606fccac41bb1d1f59e262cb19e6f5b687a5
Diego <diegosantos4957@gmail.com>
2025-07-31 22:07:49 -0300

    feat: create route invite accept

--- src/modules/users/controllers/invite-users/invite-users.controller.ts
+++ src/modules/users/controllers/invite-users/invite-users.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Param } from '@nestjs/common';
 import { InviteUserPropertyDto } from '../../dto/invite-user-property.dto';
-import { InviteUserService } from '../../services/invite-user/invite-user.service';
+import { SendInviteUserService } from '../../services/send-invite-user/send-invite-user.service';
 
 @Controller('invite-users')
 export class InviteUsersController {
-  constructor(private readonly inviteUserService: InviteUserService) {}
+  constructor(private readonly inviteUserService: SendInviteUserService) {}
 
   @Post('/')
   public async inviteUserByProperty(@Body() inviteUser: InviteUserPropertyDto) {
@@ -17,5 +17,12 @@ export class InviteUsersController {
     await this.inviteUserService.execute(invite);
 
     return { message: 'This action invites a user by property 2' };
+  }
+
+  @Post('/accept/:invite_id')
+  public acceptInvite(@Param('invite_id') invite_id: string) {
+    /* await this.inviteUserService.acceptInvite(invite_id);*/
+    console.log(invite_id);
+    return { message: 'Invite accepted successfully' };
   }
 }
