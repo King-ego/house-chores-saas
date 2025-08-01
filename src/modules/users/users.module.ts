a6f7de007d17eb67a73dab16904416f00ce7c7a4
Diego <diegosantos4957@gmail.com>
2025-07-31 22:08:42 -0300

    feat: update depends

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,7 +3,7 @@ import { UsersController } from './controllers/users/users.controller';
 import { UsersRepositoryModule } from './repositories/users.repository.module';
 import { ListUsersByIdService } from './services/list-users-by-id/list-users-by-id.service';
 import { CreateUserService } from './services/create-user/create-user.service';
-import { InviteUserService } from './services/invite-user/invite-user.service';
+import { SendInviteUserService } from './services/send-invite-user/send-invite-user.service';
 import { InviteUsersController } from './controllers/invite-users/invite-users.controller';
 import { NotificationsRepositoryModule } from '../notifications/repositories/notifications.repository.module';
 import { PasswordService } from './services/password/password.service';
@@ -13,7 +13,7 @@ import { PasswordService } from './services/password/password.service';
   providers: [
     ListUsersByIdService,
     CreateUserService,
-    InviteUserService,
+    SendInviteUserService,
     PasswordService,
   ],
   imports: [UsersRepositoryModule, NotificationsRepositoryModule],
