f627474f6f1267ef8263a305f4548800f20dcfa2
Diego <diegosantos4957@gmail.com>
2025-08-01 17:05:47 -0300

    feat: update invite user in service

--- src/modules/users/services/accept-invite/accept-invite.service.ts
+++ src/modules/users/services/accept-invite/accept-invite.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { InviteUsersRepository } from '../../repositories/invite-users.repository';
+import { CustomerException } from '../../../../shared/errors/customerException';
+import { UsersRepository } from '../../repositories/users.repository';
 
 @Injectable()
 export class AcceptInviteService {
-  constructor(private readonly inviteUserRepository: InviteUsersRepository) {}
+  constructor(
+    private readonly inviteUserRepository: InviteUsersRepository,
+    private readonly userRepository: UsersRepository,
+  ) {}
 
   public async execute(invite_id: string) {
-    //continuar aqui
     const invite = await this.inviteUserRepository.inviteUserById(invite_id);
+    if (!invite) {
+      throw new CustomerException(`Invite with id ${invite_id} not found`, 404);
+    }
+    const user = await this.userRepository.findByFilter({
+      email: invite.email,
+    });
 
+    await this.inviteUserRepository.acceptInvite({
+      user_id: user.id,
+      property_id: invite.property_id,
+    });
+
+    await this.inviteUserRepository.deleteInviteUser(invite_id);
   }
 }
