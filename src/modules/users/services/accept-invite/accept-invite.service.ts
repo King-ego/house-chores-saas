b72bd25bf2b2e23e852e9c219a6cadbfd8dfd276
Diego <diegosantos4957@gmail.com>
2025-07-31 22:23:30 -0300

    feat: list invite user in accept invite

---
+++ src/modules/users/services/accept-invite/accept-invite.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { InviteUsersRepository } from '../../repositories/invite-users.repository';
+
+@Injectable()
+export class AcceptInviteService {
+  constructor(private readonly inviteUserRepository: InviteUsersRepository) {}
+
+  public async execute(invite_id: string) {
+    const invite = await this.inviteUserRepository.inviteUserById(invite_id);
+
+  }
+}
