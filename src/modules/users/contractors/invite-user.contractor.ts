1f183a2cf0bcfd7b224ee427cf95932417852724
Diego <diegosantos4957@gmail.com>
2025-07-31 22:19:20 -0300

    feat: add method find invite user

--- src/modules/users/contractors/invite-user.contractor.ts
+++ src/modules/users/contractors/invite-user.contractor.ts
@@ -1,5 +1,7 @@
 import { CreateInviteUserInput } from './inputs/invite-user-property-inputs';
+import { InviteUser } from '../../../../prisma/generated/client/postgres';
 
 export interface InviteUserContractor {
   createInviteUser: (createInvite: CreateInviteUserInput) => Promise<void>;
+  inviteUserById: (invite_id: string) => Promise<InviteUser | null>;
 }
