3957dbe855e1ac9be5e01a982abc8125af191fef
Diego <diegosantos4957@gmail.com>
2025-08-02 18:06:36 -0300

    feat: export create auth service

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,9 +3,8 @@ import { AuthController } from './controllers/auth/auth.controller';
 import { CreateAuthService } from './services/create-auth/create-auth.service';
 
 @Module({
-
   controllers: [AuthController],
-
-  providers: [CreateAuthService]
+  providers: [CreateAuthService],
+  exports: [CreateAuthService],
 })
 export class AuthModule {}
