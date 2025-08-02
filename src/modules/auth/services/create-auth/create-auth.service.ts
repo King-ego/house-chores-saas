8ae83a6942087dc55857743b572db5924aa4aa5b
Diego <diegosantos4957@gmail.com>
2025-08-02 18:28:37 -0300

    feat: update create auth service

--- src/modules/auth/services/create-auth/create-auth.service.ts
+++ src/modules/auth/services/create-auth/create-auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+interface IUser {
+  email: string;
+  password: string;
+}
 
 @Injectable()
-export class CreateAuthService {}
+export class CreateAuthService {
+  constructor(private readonly jwtService: JwtService,) {}
+
+  public async login(user: IUser) {
+    /*const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };*/
+  }
+}
