e5c9f178ff1936d0bcbf1a25acf1bc6dcbccc223
Diego <diegosantos4957@gmail.com>
2025-08-02 17:52:44 -0300

    feat: new module auth in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { PrismaModule } from './shared/prisma/prisma.module';
 import { UsersModule } from './modules/users/users.module';
 import { PropertiesModule } from './modules/properties/properties.module';
 import { NotificationsModule } from './modules/notifications/notifications.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, PropertiesModule, NotificationsModule],
+  imports: [
+    PrismaModule,
+    UsersModule,
+    PropertiesModule,
+    NotificationsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
