ec1c7b50902e45dbe27e9fec09f31eda4b7ff4fb
Diego <diegosantos4957@gmail.com>
2025-08-02 17:56:05 -0300

    feat: add crate auth in auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth/auth.controller';
+import { CreateAuthService } from './services/create-auth/create-auth.service';
 
 @Module({
 
-  controllers: [AuthController]
+  controllers: [AuthController],
+
+  providers: [CreateAuthService]
 })
 export class AuthModule {}
