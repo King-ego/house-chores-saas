b03a408ed95fded49cf93fdb16a542e7cd096d9c
Diego <diegosantos4957@gmail.com>
2025-08-03 16:42:46 -0300

    feat: config module auth

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth/auth.controller';
 import { CreateAuthService } from './services/create-auth/create-auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'default_secret_key',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [CreateAuthService],
   exports: [CreateAuthService],
