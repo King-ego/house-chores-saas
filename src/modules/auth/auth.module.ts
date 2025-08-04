180f65da77d27a04cdc5aaf957195547e37a7735
Diego <diegosantos4957@gmail.com>
2025-08-03 21:43:42 -0300

    feat: import users repository in auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth/auth.controller';
 import { CreateAuthService } from './services/create-auth/create-auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { UsersRepositoryModule } from '../users/repositories/users.repository.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { JwtModule } from '@nestjs/jwt';
       secret: process.env.JWT_SECRET || 'default_secret_key',
       signOptions: { expiresIn: '1d' },
     }),
+    UsersRepositoryModule,
   ],
   controllers: [AuthController],
   providers: [CreateAuthService],
