e6b137666e188bd2232f6c8af61d06771b692b48
Diego <diegosantos4957@gmail.com>
2025-08-04 22:08:06 -0300

    feat: verify user exist

--- src/modules/users/services/create-user/create-user.service.ts
+++ src/modules/users/services/create-user/create-user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { UsersRepository } from '../../repositories/users.repository';
 import { User } from '../../../../../prisma/generated/client/postgres';
 import { PasswordService } from '../password/password.service';
+import { CustomerException } from '../../../../shared/errors/customerException';
 
 interface CreateUserRequest {
   name: string;
@@ -17,6 +18,12 @@ export class CreateUserService {
   ) {}
 
   public async execute(user: CreateUserRequest): Promise<User> {
+    const existingUser = await this.usersRepository.findByFilter({
+      email: user.email,
+    });
+    if (existingUser) {
+      throw new CustomerException('User with this email already exists', 400);
+    }
     user.password = await this.passwordService.hashPassword(user.password);
     return this.usersRepository.createUser(user);
   }
