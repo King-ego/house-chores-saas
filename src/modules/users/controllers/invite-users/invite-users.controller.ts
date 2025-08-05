e151bf92dc215f9f2aeeb0ce6fbee0e87f7d9fe8
Diego <diegosantos4957@gmail.com>
2025-08-04 22:10:22 -0300

    feat: add auth in accept invite

--- src/modules/users/controllers/invite-users/invite-users.controller.ts
+++ src/modules/users/controllers/invite-users/invite-users.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post, Param } from '@nestjs/common';
+import { Body, Controller, Post, Param, UseGuards } from '@nestjs/common';
 import { InviteUserPropertyDto } from '../../dto/invite-user-property.dto';
 import { SendInviteUserService } from '../../services/send-invite-user/send-invite-user.service';
 import { AcceptInviteService } from '../../services/accept-invite/accept-invite.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('invite-users')
 export class InviteUsersController {
@@ -22,7 +23,7 @@ export class InviteUsersController {
 
     return { message: 'This action invites a user by property' };
   }
-
+  @UseGuards(AuthGuard('jwt'))
   @Post('/accept/:invite_id')
   public async acceptInvite(@Param('invite_id') invite_id: string) {
     await this.acceptInviteService.execute(invite_id);
