1375b048ae710a412a02ef391aefb48b9d6e4e9f
Diego <diegosantos4957@gmail.com>
2025-08-05 22:17:05 -0300

    feat: update path users repository in list users

--- src/modules/users/services/list-users-by-id/list-users-by-id.service.ts
+++ src/modules/users/services/list-users-by-id/list-users-by-id.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { UsersRepository } from '../../repositories/users.repository';
+import { UsersRepository } from '../../repositories/users/users.repository';
 import { User } from '../../../../../prisma/generated/client/postgres';
 
 @Injectable()
