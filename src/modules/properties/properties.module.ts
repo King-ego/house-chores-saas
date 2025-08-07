3f8b8e6a94106120d8055a34c0ca2796486eca5c
Diego <diegosantos4957@gmail.com>
2025-08-06 21:52:52 -0300

    feat: update property module

--- src/modules/properties/properties.module.ts
+++ src/modules/properties/properties.module.ts
@@ -3,10 +3,11 @@ import { PropertiesController } from './controllers/properties/properties.contro
 import { CreatePropertiesService } from './services/create-properties/create-properties.service';
 import { PropertiesRepositoryModule } from './repositories/properties.repository.module';
 import { ListPropertyByUserIdService } from './services/list-property-by-user-id/list-property-by-user-id.service';
+import { DeletePropertyService } from './services/delete-property/delete-property.service';
 
 @Module({
   controllers: [PropertiesController],
-  providers: [CreatePropertiesService, ListPropertyByUserIdService],
+  providers: [CreatePropertiesService, ListPropertyByUserIdService, DeletePropertyService],
   imports: [PropertiesRepositoryModule],
 })
 export class PropertiesModule {}
