03ac453c0a0a849361034c9329bb279be43133cd
Diego <diegosantos4957@gmail.com>
2025-08-06 22:05:51 -0300

    feat: delete property repository

--- src/modules/properties/services/delete-property/delete-property.service.ts
+++ src/modules/properties/services/delete-property/delete-property.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PropertyRepositories } from '../../repositories/properties/property.repositories';
+import { CustomerException } from '../../../../shared/errors/customerException';
 
 @Injectable()
-export class DeletePropertyService {}
+export class DeletePropertyService {
+  constructor(private readonly propertiesRepository: PropertyRepositories) {}
+
+  public async execute(property_id: string): Promise<void> {
+    const property = await this.propertiesRepository.findByFilter({
+      property_id,
+    });
+
+    if (!property) {
+      throw new CustomerException('Property not found', 404);
+    }
+
+    await this.propertiesRepository.delete_property({ property_id });
+  }
+}
