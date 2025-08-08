cc293976b33ae0becb5434eb7e53c5ccf5362bb6
Diego <diegosantos4957@gmail.com>
2025-08-07 22:17:06 -0300

    feat: update notification module

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { NotificationsController } from './controllers/notifications/notifications.controller';
 import { ListNotificationsIdService } from './services/list-notifications-id/list-notifications-id.service';
 import { NotificationsRepositoryModule } from './repositories/notifications.repository.module';
+import { DeleteNotificationService } from './services/delete-notification/delete-notification.service';
 
 @Module({
   controllers: [NotificationsController],
-  providers: [ListNotificationsIdService],
+  providers: [ListNotificationsIdService, DeleteNotificationService],
   imports: [NotificationsRepositoryModule],
 })
 export class NotificationsModule {}
