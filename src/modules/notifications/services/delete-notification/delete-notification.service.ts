976102202702c3d0c2c99a3d1218b2958eafb565
Diego <diegosantos4957@gmail.com>
2025-08-07 22:30:11 -0300

    feat: update delete notification services

--- src/modules/notifications/services/delete-notification/delete-notification.service.ts
+++ src/modules/notifications/services/delete-notification/delete-notification.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../../repositories/notifications/notifications.repository';
 
 @Injectable()
-export class DeleteNotificationService {}
+export class DeleteNotificationService {
+  constructor(
+    private readonly notificationsRepository: NotificationsRepository,
+  ) {}
+
+  public async execute(notificationId: string): Promise<void> {
+    await this.notificationsRepository.deleteNotification(notificationId);
+  }
+}
