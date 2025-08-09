f1848c3beda65df77247f9a0a647e1ce430417f0
Diego <diegosantos4957@gmail.com>
2025-08-08 22:03:22 -0300

    feat: new method read notification contractor

--- src/modules/notifications/contractors/notifications-contractors.ts
+++ src/modules/notifications/contractors/notifications-contractors.ts
@@ -1,9 +1,10 @@
 import { Notification } from '../../../../prisma/generated/client/postgres';
 import { CreateNotificationInput } from './inputs/create.notification.input';
+import { ReadNotificationInput } from './inputs/read-notification.input';
 
 export interface NotificationsContractors {
   listNotificationsByUserId(userId: string): Promise<Notification[]>;
   createNotification(_: CreateNotificationInput): Promise<void>;
   deleteNotification(notificationId: string): Promise<void>;
-  readNotification(notificationId: string): Promise<void>;
+  readNotification(_: ReadNotificationInput): Promise<void>;
 }
