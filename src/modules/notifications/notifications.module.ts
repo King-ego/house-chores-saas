d716ebe3667fac811eb615b181fa3624d420706f
Diego <diegosantos4957@gmail.com>
2025-08-08 22:07:02 -0300

    feat: update module notification

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -3,10 +3,11 @@ import { NotificationsController } from './controllers/notifications/notificatio
 import { ListNotificationsIdService } from './services/list-notifications-id/list-notifications-id.service';
 import { NotificationsRepositoryModule } from './repositories/notifications.repository.module';
 import { DeleteNotificationService } from './services/delete-notification/delete-notification.service';
+import { ReadNotificationService } from './services/read-notification/read-notification.service';
 
 @Module({
   controllers: [NotificationsController],
-  providers: [ListNotificationsIdService, DeleteNotificationService],
+  providers: [ListNotificationsIdService, DeleteNotificationService, ReadNotificationService],
   imports: [NotificationsRepositoryModule],
 })
 export class NotificationsModule {}
