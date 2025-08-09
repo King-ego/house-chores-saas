25e95b65dc46b33a055d7c5182e0045ef692a710
Diego <diegosantos4957@gmail.com>
2025-08-08 22:09:45 -0300

    feat: update module read notification services

--- src/modules/notifications/services/read-notification/read-notification.service.ts
+++ src/modules/notifications/services/read-notification/read-notification.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../../repositories/notifications/notifications.repository';
+
+interface CreateNotificationInput {
+  notificationId: string;
+  read: boolean;
+}
 
 @Injectable()
-export class ReadNotificationService {}
+export class ReadNotificationService {
+  constructor(
+    private readonly notificationRepository: NotificationsRepository,
+  ) {}
+
+  public async execute(data: CreateNotificationInput): Promise<void> {
+    const { notificationId, read } = data;
+
+    await this.notificationRepository.readNotification({
+      notificationId,
+      read,
+    });
+  }
+}
