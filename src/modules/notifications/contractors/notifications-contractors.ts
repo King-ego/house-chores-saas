1b4ec2ca45e83f41f0fd442baaab8660883615a7
Diego <diegosantos4957@gmail.com>
2025-08-09 22:13:34 -0300

    feat: change name input notification contractor

--- src/modules/notifications/contractors/notifications-contractors.ts
+++ src/modules/notifications/contractors/notifications-contractors.ts
@@ -1,11 +1,12 @@
 import { Notification } from '../../../../prisma/generated/client/postgres';
 import { CreateNotificationInput } from './inputs/create.notification.input';
 import { ReadNotificationInput } from './inputs/read-notification.input';
+import { FindByFilterNotification } from './inputs/findByFIlterNotification';
 
 export interface NotificationsContractors {
   listNotificationsByUserId(userId: string): Promise<Notification[]>;
   createNotification(_: CreateNotificationInput): Promise<void>;
   deleteNotification(notificationId: string): Promise<void>;
   readNotification(_: ReadNotificationInput): Promise<void>;
-  findByFilter(): Promise<Notification>;
+  findByFilter(_: FindByFilterNotification): Promise<Notification>;
 }
